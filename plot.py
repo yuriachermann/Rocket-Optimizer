b6d34d3ff256d4f9d4471afc81fcb4fe1f9ff728
Yuri Winche Achermann <yuri_wa@hotmail.com>
2021-08-22 01:53:49 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,9 +1,9 @@
-import numpy as np
-import sys
 import os
+import sys
+import numpy as np
+from PIL import Image
 import matplotlib as m
 import matplotlib.pyplot as plt
-from PIL import Image
 
 
 def delete_images():
@@ -22,13 +22,12 @@ def delete_images():
 
 def plot_all(pos_n, n):
     ax = plt.figure().add_subplot(projection='3d')
-
-    ax.scatter(pos_n[0], pos_n[1], pos_n[2])
-    pnt3d = ax.scatter(pos_n[0], pos_n[1], pos_n[2], c=pos_n[2])
+    ax.scatter(pos_n[0], pos_n[1], pos_n[2], c=pos_n[2])
 
     ax.azim = 120  # isomeric=120 | superior=0
     ax.dist = 10  # standard=10
     ax.elev = 30  # isomeric=30 | superior=90
+
     ax.set_xlim3d(-0.01, 0.07)
     ax.set_ylim3d(-0.01, 0.11)
     ax.set_zlim3d(1400, 1600)
@@ -37,14 +36,11 @@ def plot_all(pos_n, n):
     return
 
 
-def make_gif():
+def make_gif(n):
     frames = []
-    i = 0
-    for filename in os.listdir('images'):
-        # file_path = os.path.join(folder, filename)
+    for i in range(n):
         frames.append(Image.open("images/iteration_%s.png" % i))
-        i = i + 1
 
     frame_one = frames[0]
-    frame_one.save("pso.gif", format="GIF", append_images=frames,
-                   save_all=True, duration=100, loop=0)
+    frame_one.save("pso.gif", format="GIF", append_images=frames, save_all=True, duration=100, loop=0)
+    return
